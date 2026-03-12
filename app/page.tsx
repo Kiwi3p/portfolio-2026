0f0d95dd580b1b09e7eaa6abaa97d3ce5f0f7df4
Jack Transue <johnktransue@gmail.com>
2026-03-12 09:44:09 -0400

    push test

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,6 @@ export default async function Home() {
         </AnimatedSection>
         <AnimatedSection>
           <Work projects={projects} />
-          
         </AnimatedSection>
         <AnimatedSection>
           <CTA
